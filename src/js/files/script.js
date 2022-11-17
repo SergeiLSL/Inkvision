1a454e3195af5a0c9fa5b3ebe5a9fe60d7986e22
Sergei Student <lsl1959@mail.ru>
2022-11-17 22:57:35 +0600

    fhinish gender

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,21 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+//========================================================================================================================================================
+// .gender
+// ? Группа радио кнопок Определяем пол человека
+let all = document.querySelectorAll('.r-all');
+for (let a = 0; a < all.length; a++){ 
+    let radios = all[a].querySelectorAll('.r-input');
+    let i = 1;
+    all[a].style.setProperty('--options',radios.length);
+    radios.forEach((input)=>{
+        input.setAttribute('data-pos',i);
+        input.addEventListener('click',(e)=>{
+            all[a].style.setProperty('--options-active',e.target.getAttribute('data-pos'));
+        });
+        i++;
+    });
+};
+//========================================================================================================================================================
