aa9fc9d9635d9727dea491c4dd0311638d59b3a9
Sergei Student <lsl1959@mail.ru>
2022-12-04 21:45:26 +0600

    finish страницы Гдавная (home)

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,18 +6,41 @@ import { flsModules } from "./modules.js";
 //========================================================================================================================================================
 // .gender
 // ? Группа радио кнопок Определяем пол человека
-let all = document.querySelectorAll('.r-all');
+let all = document.querySelectorAll(".r-all");
 
-for (let a = 0; a < all.length; a++){ 
-    let radios = all[a].querySelectorAll('.r-input');
+for (let a = 0; a < all.length; a++) {
+    let radios = all[a].querySelectorAll(".r-input");
     let i = 1;
-    all[a].style.setProperty('--options',radios.length);
-    radios.forEach((input)=>{
-        input.setAttribute('data-pos',i);
-        input.addEventListener('click',(e)=>{
-            all[a].style.setProperty('--options-active',e.target.getAttribute('data-pos'));
+    all[a].style.setProperty("--options", radios.length);
+    radios.forEach((input) => {
+        input.setAttribute("data-pos", i);
+        input.addEventListener("click", (e) => {
+        all[a].style.setProperty(
+            "--options-active",
+            e.target.getAttribute("data-pos")
+        );
         });
         i++;
     });
-};
+}
 //========================================================================================================================================================
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    sliderPerView: 1,
+    spaceBetween: 10,
+
+    navigation: {
+        nextE1: "swiper-button-next",
+        prevE1: "swiper-button-prev",
+    },
+    // Чувствительность свайпа
+    touchRatio: 1,
+    // Угол срабатывания свайпа
+    touchФтпду: 45,
+    // Курсор перетаскивания
+    grabCursor: true,
+    // переключение при клике на слайд
+    slideClickedSlide: true,
+
+});
